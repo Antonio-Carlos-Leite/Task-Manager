11409f521c08042029982b0a46fa552d7f9bbfc7
Matheus Barros Lima <matheusmatheusbarroslima6@gmail.com>
2024-06-12 15:36:56 -0300

    aaa

--- src/main.tsx
+++ src/main.tsx
@@ -3,21 +3,19 @@ import ReactDOM from "react-dom/client";
 import { AppRoutes } from "./routes/index.tsx";
 import { ThemeProvider } from "styled-components";
 import { appTheme } from "./styles/theme.ts";
-import { GlobalStyles } from "./styles/Global.ts";
 import { AuthProvider } from "./contexts/authContext.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { GlobalStyles } from "./styles/Global.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={new QueryClient()}>
-      
       <AuthProvider>
         <ThemeProvider theme={appTheme}>
           <AppRoutes />
           <GlobalStyles />
         </ThemeProvider>
       </AuthProvider>
-    
     </QueryClientProvider>
   </React.StrictMode>
 );
