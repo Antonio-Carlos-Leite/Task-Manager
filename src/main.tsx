52f36ac9e09625bd35dcafe172966adb1f5f36e3
Matheus Barros Lima <matheusmatheusbarroslima6@gmail.com>
2024-06-12 15:45:35 -0300

    qqqq

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from "styled-components";
 import { appTheme } from "./styles/theme.ts";
 import { AuthProvider } from "./contexts/authContext.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { GlobalStyles } from "./styles/global.ts";
+import { GlobalStyles } from "./styles/Global.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
